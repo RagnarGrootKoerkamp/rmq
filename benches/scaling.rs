de21ebef2bb1977e973fe50a3842dc589f486e8d
Leandro Conte <me@lcjconte.com>
2026-09-24 14:00:56 +0200

    Added index sparse table and benchmarks

--- benches/scaling.rs
+++ benches/scaling.rs
@@ -2,9 +2,11 @@ use criterion::{
     AxisScale, BatchSize, BenchmarkId, Criterion, PlotConfiguration, criterion_group,
     criterion_main,
 };
+use rand::{RngExt, SeedableRng, rngs::Xoshiro256PlusPlus};
+use rmq::{base::RMQ, rmq_tree::RMQTree, sparse_table::OffsetSparseTable};
 
-fn scaling(c: &mut Criterion) {
-    let mut group = c.benchmark_group("sort");
+fn scaling_sparse_table(c: &mut Criterion) {
+    let mut group = c.benchmark_group("sparse_table");
     group.plot_config(PlotConfiguration::default().summary_scale(AxisScale::Logarithmic));
 
     // Half-decade steps: 10, 31, 100, 316, ..., 1_000_000
@@ -15,11 +17,43 @@ fn scaling(c: &mut Criterion) {
             group.sample_size(10); // minimum allowed; keeps big sizes from taking forever
         }
         let data: Vec<u64> = (0..n as u64).rev().collect();
+        let mut rng = Xoshiro256PlusPlus::seed_from_u64(42);
 
         group.bench_with_input(BenchmarkId::from_parameter(n), &data, |b, data| {
             b.iter_batched(
-                || data.clone(),  // setup, not timed
-                |mut v| v.sort(), // timed
+                || OffsetSparseTable::new(&data),  // setup, not timed
+                |v| {
+                    let l = rng.random_range(0..n);
+                    let r = rng.random_range(l..n);
+                    v.rmq(l, r);
+                }, // timed
+                BatchSize::LargeInput,
+            )
+        });
+    }
+    group.finish();
+
+    group = c.benchmark_group("rmq_tree");
+    group.plot_config(PlotConfiguration::default().summary_scale(AxisScale::Logarithmic));
+
+    // Half-decade steps: 10, 31, 100, 316, ..., 1_000_000
+    let sizes = (2..=12).map(|i| 10f64.powf(i as f64 / 2.0) as usize);
+
+    for n in sizes {
+        if n >= 100_000 {
+            group.sample_size(10); // minimum allowed; keeps big sizes from taking forever
+        }
+        let data: Vec<u64> = (0..n as u64).rev().collect();
+        let mut rng = Xoshiro256PlusPlus::seed_from_u64(42);
+
+        group.bench_with_input(BenchmarkId::from_parameter(n), &data, |b, data| {
+            b.iter_batched(
+                || RMQTree::new(&data),  // setup, not timed
+                |v| {
+                    let l = rng.random_range(0..n);
+                    let r = rng.random_range(l..n);
+                    v.rmq(l, r);
+                }, // timed
                 BatchSize::LargeInput,
             )
         });
@@ -27,5 +61,5 @@ fn scaling(c: &mut Criterion) {
     group.finish();
 }
 
-criterion_group!(benches, scaling);
+criterion_group!(benches, scaling_sparse_table);
 criterion_main!(benches);
--- src/sparse_table.rs
+++ src/sparse_table.rs
@@ -1,4 +1,4 @@
-use crate::base::{RMQ, RMQFamily};
+use crate::base::{RMQ, RMQFamily, TwoArgMin};
 
 pub struct OffsetSparseTable<'a> {
     n: usize,
@@ -50,13 +50,7 @@ impl<'a> RMQ<'a, u64> for OffsetSparseTable<'a> {
         let left_index = self.offsets[(k - 1) * self.n + l] + l;
         let right_index = self.offsets[(k - 1) * self.n + r + 1 - (1 << k)] + r + 1 - (1 << k);
 
-        let left = self.data[left_index];
-        let right = self.data[right_index];
-        if left <= right {
-            left_index
-        } else {
-            right_index
-        }
+        self.data.argmin(left_index, right_index)
     }
 }
 
@@ -66,4 +60,45 @@ pub struct SparseTableFamily;
 
 impl RMQFamily<u64> for SparseTableFamily {
     type Rmq<'a> = OffsetSparseTable<'a>;
+}
+
+pub struct IndexSparseTable<'a> {
+    n: usize,
+    k_max: usize,
+    data: &'a [u64],
+    indices: Vec<usize>,
+}
+
+impl<'a> IndexSparseTable<'a> {
+    pub fn new(data: &'a [u64], indices: &[usize]) -> Self {
+        let n = indices.len();
+        let k_max = n.ilog2() as usize;
+        let mut table = Self {
+            n,
+            k_max,
+            data,
+            indices: vec![0; (k_max+1) * n],
+        };
+        
+        for i in 0..n {
+            table.indices[i] = indices[i];
+        }
+        for k in 1..=table.k_max {
+            for i in 0..table.n + 1 - (1 << k) {
+                let lindex = table.indices[(k - 1) * table.n + i];
+                let rindex = table.indices[(k - 1) * table.n + i + (1 << (k - 1))];
+                table.indices[k * n + i] = data.argmin(lindex, rindex);
+            }
+        }
+        table
+    }
+
+    // Queries [l, r]
+    pub fn rmq(&self, l: usize, r: usize) -> usize {
+        let k = (r - l + 1).ilog2() as usize;
+        let left_index = self.indices[k * self.n + l];
+        let right_index = self.indices[k * self.n + r + 1 - (1 << k)];
+
+        self.data.argmin(left_index, right_index)
+    }
 }
